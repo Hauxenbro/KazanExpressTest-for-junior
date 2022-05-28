5a58a0b5932bf6c9bba6fc07a7f9be66c3141473
Hauxenbro <akuzmenko11@gmail.com>
2022-05-28 17:25:06 +0300

    Update

--- KazanExpress/API/admin.py
+++ KazanExpress/API/admin.py
@@ -14,13 +14,13 @@ class ProductsAdmin(admin.ModelAdmin):
     readonly_fields = ('id',)
     list_display = ['id', 'title', 'amount', 'price', 'active']
     list_display_links = ['title', 'amount', 'price', 'active']
-    search_fields = ('title',)
+    search_fields = ('title', 'id')
 
 class CategoryAdmin(MPTTModelAdmin):
     readonly_fields = ('id',)
     list_display = ['id','title']
     list_display_links = ['title']
-    search_fields = ('title',)
+    search_fields = ('title', 'id', 'parent')
 
 
 admin.site.register(Shop, ShopAdmin)
